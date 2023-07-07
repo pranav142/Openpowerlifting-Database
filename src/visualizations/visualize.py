1fa38a9ad3d1f86b3c230955548049ccae5e40f2
pranav142 <pknadimp@gmail.com>
2023-07-06 22:24:35 -0700

    More visualizations

--- src/visualizations/visualize.py
+++ src/visualizations/visualize.py
@@ -1,3 +1,65 @@
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_bar_graph(
+    *, data: pd.core.series.Series, title: str, xlabel: str, ylabel: str, kind: str
+):
+    """Plots a bargraph using data"""
+    plt.figure(figsize=(10, 7))
+    data.plot(kind=kind, color=plt.cm.Set3(range(10)))
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+
+def plot_pie_chart(
+    *,
+    data: pd.core.series.Series,
+    title: str,
+    digit_format: str = "%1.1f%%",
+):
+    """Plots a pie chart"""
+    plt.figure(figsize=(10, 7))
+    plt.pie(data.values, labels=data.index, autopct=digit_format)
+    plt.axis("equal")
+    plt.legend(title="Categories", loc="best")
+    plt.title(title)
+    plt.show()
+
+
+def plot_histogram(
+    *,
+    data: pd.DataFrame,
+    title: str,
+    bins: int,
+    xlabel: str,
+    ylabel: str,
+    hue: str = None,
+    column: str,
+):
+    """Plots a histogram"""
+    assert column in data.columns, "column not found in dataframe"
+    ALPHA = 0.4
+    plt.figure(figsize=(10, 7))
+
+    if hue is None:
+        plt.hist(data[column], bins=bins, alpha=ALPHA)
+    else:
+        unique_hues = data[hue].unique()
+        for hue_value in unique_hues:
+            plt.hist(
+                data[data[hue] == hue_value][column],
+                bins=bins,
+                alpha=ALPHA,
+                label=hue_value,
+            )
+        plt.legend()
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
